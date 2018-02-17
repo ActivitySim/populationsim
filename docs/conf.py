80fe4bc243fff8728b0cbb51654e018b99934926
bstabler <benstabler@yahoo.com>
2018-02-16 16:47:33 -0800

    get package version from setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,14 @@ import os
 
 import sphinx_rtd_theme
 
+# -- Get Package Version --------------------------------------------------
+with open("../setup.py") as file:
+    lines = file.readlines()
+    for l in lines:
+        if "version" in l:
+            VERSION = l.replace("version='", "").replace("',", "").replace(" ", "")
+            print("package version: " + VERSION)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
