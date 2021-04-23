1b72fbc6329271254f045aa3a896db470965407f
bstabler <benstabler@yahoo.com>
2021-04-23 11:26:02 -0700

    use latest asim

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(exclude=['*.tests']),
     include_package_data=True,
     install_requires=[
-        'activitysim >= 0.9.9',
+        'activitysim >= 0.9.9.1',
         'numpy >= 1.16.1',
         'pandas >= 1.1.0',
         'ortools >= 5.1.4045'
