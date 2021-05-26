65fbd3212e698b37f7260a6bb50ebc062b737eec
leah.flake <leah.flake@rsginc.com>
2021-05-26 11:04:15 -0400

    added arguments to repop balancing step

--- populationsim/steps/initial_seed_balancing.py
+++ populationsim/steps/initial_seed_balancing.py
@@ -74,7 +74,7 @@ def initial_seed_balancing(settings, crosswalk, control_spec, incidence_table):
 
     seed_ids = crosswalk_df[seed_geography].unique()
     for seed_id in seed_ids:
-  
+
         logger.info("initial_seed_balancing seed id %s" % seed_id)
 
         seed_incidence_df = incidence_df[incidence_df[seed_geography] == seed_id]
--- populationsim/steps/repop_balancing.py
+++ populationsim/steps/repop_balancing.py
@@ -60,6 +60,8 @@ def repop_balancing(settings, crosswalk, control_spec, incidence_table):
 
     max_expansion_factor = settings.get('max_expansion_factor', None)
     min_expansion_factor = settings.get('min_expansion_factor', None)
+    absolute_upper_bound = settings.get('absolute_upper_bound', None)
+    absolute_lower_bound = settings.get('absolute_lower_bound', None)
 
     # run balancer for each low geography
     low_weight_list = []
@@ -100,7 +102,9 @@ def repop_balancing(settings, crosswalk, control_spec, incidence_table):
                 control_spec=low_control_spec,
                 total_hh_control_col=total_hh_control_col,
                 max_expansion_factor=max_expansion_factor,
-                min_expansion_factor=min_expansion_factor,
+                min_expansion_factor=min_expansion_factor,  
+                absolute_upper_bound=absolute_upper_bound,
+                absolute_lower_bound=absolute_lower_bound,
                 incidence_df=seed_incidence_df,
                 control_totals=low_controls_df.loc[low_id],
                 initial_weights=initial_weights)
