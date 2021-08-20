c3d8c1ec0a858fce6e39697d708445412d00d968
leah.flake <leah.flake@rsginc.com>
2021-08-20 08:33:54 -0400

    removing white space

--- populationsim/balancer.py
+++ populationsim/balancer.py
@@ -296,7 +296,7 @@ def do_balancing(control_spec,
             ub_weights = ub_weights.round().clip(upper=absolute_upper_bound, lower=1).astype(int)
         else:
             ub_weights = ub_weights.round().clip(lower=1).astype(int)
-    
+
     elif absolute_upper_bound:
         ub_weights = ub_weights.round().clip(upper=absolute_upper_bound, lower=1).astype(int)
 
