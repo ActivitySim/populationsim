47ece6651a1b41e4ddd733a4142e12dd29f17cf9
Ben Stabler <ben.stabler@rsginc.com>
2021-08-26 09:01:27 -0700

    publish v0.5.1 (#141)
    * update package version number as well
    
    * Allow non-binary incidence (#123)
    
    * Allow non-binary incidence
    
    * style
    
    * update tests to pass
    
    * add some progress indication
    
    * tidy up validation script, use histogram for a histogram
    
    * fix render and some typos
    
    * increment version
    
    * deprecate py2.7
    
    * Multiprocess (#130)
    
    * [Bugfix] Allow seed and meta geography to be the same (#139)
    
    * Fixes bug where if the seed geography is the same as the meta_geography, pandas has a small panic attack and the run will fail.
    
    * add cytoolz to the "requirements"
    
    * fix another activitysim change
    
    * Absolute bounds (#136)
    
    
    * adding upper/lower bounds to weighting use case
    
    * #137, #134, #133, #131
    
    Co-authored-by: Jamie Cook <jamie.cook@veitchlister.com.au>
    Co-authored-by: Blake Rosenthal <blake.rosenthal@rsginc.com>
    Co-authored-by: Ben Stabler <bstabler@users.noreply.github.com>
    Co-authored-by: Leah Flake <leah.flake@rsginc.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='populationsim',
-    version='0.5',
+    version='0.5.1',
     description='Population Synthesis',
     author='contributing authors',
     author_email='ben.stabler@rsginc.com',
