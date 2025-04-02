9e6dee5dadf2984a08bda187edc56d265679f434
nick-fournier-rsg <nick.fournier@rsginc.com>
2025-04-02 13:30:25 -0700

    add oceanside repop example

---
+++ example_oceanside_repop/run_populationsim.py
@@ -0,0 +1,41 @@
+import sys
+import argparse
+from pathlib import Path
+
+from activitysim.core.config import setting
+from activitysim.core import inject
+
+from activitysim.cli.run import add_run_args, run
+from populationsim import steps
+from construct_pipe import ConstructPipe
+
+@inject.injectable()
+def log_settings():
+
+    return [
+        'multiprocess',
+        'num_processes',
+        'resume_after',
+        'GROUP_BY_INCIDENCE_SIGNATURE',
+        'INTEGERIZE_WITH_BACKSTOPPED_CONTROLS',
+        'SUB_BALANCE_WITH_FLOAT_SEED_WEIGHTS',
+        'meta_control_data',
+        'control_file_name',
+        'USE_CVXPY',
+        'USE_SIMUL_INTEGERIZER'
+    ]
+
+
+if __name__ == '__main__':
+
+    assert inject.get_injectable('preload_injectables', None)
+
+    parser = argparse.ArgumentParser()
+    add_run_args(parser)
+    args = parser.parse_args()
+    args.working_dir = Path(__file__).parent.resolve()
+    
+    # Set up the pipeline
+    ConstructPipe(args.working_dir)
+    
+    sys.exit(run(args))
